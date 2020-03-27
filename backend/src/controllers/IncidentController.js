37b2f433099ce8f08840117876a2897f9015d94f
Andre <andredionisio12@gmail.com>
2020-03-27 20:57:19 -0300

    Front-end da aplicacao

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,8 +6,6 @@ module.exports = {
 
     const [count] = await connection("incidents").count();
 
-    console.log(count);
-
     const incidents = await connection("incidents")
       .join("ongs", "ongs.id", "=", "incidents.ong_id")
       .limit(5)
@@ -16,7 +14,7 @@ module.exports = {
         "incidents.*",
         "ongs.name",
         "ongs.email",
-        "ong.whatsapp",
+        "ongs.whatsapp",
         "ongs.city",
         "ongs.uf"
       ]);
